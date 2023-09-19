e0ab5b1edcefcfe01bc3b50ec3b8cb3dfa930b31
Çağrı <karpuzcagri@gmail.com>
2023-09-19 20:02:40 +0300

    long way to go

---
+++ libary otomation with excel/lib.py
@@ -0,0 +1,24 @@
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import Font, PatternFill
+
+
+
+wb = Workbook()
+ws = wb.active
+ws.title = "Library"
+
+ws.append(['Books Name','date of borrowing the book','date of return of the book','the person who rented the book'])
+
+book_name = input("input name a book: ")
+timein = input("the date the book was checked out: ")
+timeout = input("input the return date: ")
+name = input("Who is taking the book? ")
+
+ws["A2"]  = book_name
+ws["B2"]  = timein
+ws["C2"]  = timeout
+ws["D2"]  = name
+
+
+wb.save('library.xlsx')
